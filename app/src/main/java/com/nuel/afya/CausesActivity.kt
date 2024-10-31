fed0a10d713ede966a23601daf246b7aa3212ffd
Elisha-3 <keancheelisha3@gmail.com>
2024-10-31 12:05:44 +0300

    Afya code

---
+++ app/src/main/java/com/nuel/afya/CausesActivity.kt
@@ -0,0 +1,27 @@
+package com.nuel.afya
+
+import android.os.Bundle
+import android.webkit.WebView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class CausesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_causes)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        //        fetch webview by
+        val causesview = findViewById<WebView>(R.id.causes)
+
+//        locate the html file in the assets folder
+        causesview.loadUrl("file:///android_asset/causes.html")
+
+    }
+}
---
+++ app/src/main/java/com/nuel/afya/LocationActivity.kt
@@ -0,0 +1,26 @@
+package com.nuel.afya
+
+import android.os.Bundle
+import android.webkit.WebView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LocationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_location)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        //        fetch webview by
+        val locationview = findViewById<WebView>(R.id.location)
+
+//        locate the html file in the assets folder
+        locationview.loadUrl("file:///android_asset/location.html")
+    }
+}
